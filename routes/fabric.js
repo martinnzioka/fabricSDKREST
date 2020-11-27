773392a446829a28638b0b98d4454b84571cb44b
martinmutua <martin.mutua@protonmail.com>
2020-11-27 09:28:47 +0300

    updated

--- routes/fabric.js
+++ routes/fabric.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const {} = require('../fabricsdk/fabric');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
